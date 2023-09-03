21d118c043e93084d2cb3e88c37b630aa548a008
dmitriydmx <dmitriydmx@mail.ru>
2023-09-03 11:07:07 +0900

    first commit mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,43 @@
+const holes = document.querySelectorAll('.hole');
+const deadCounter = document.getElementById('dead');
+const lostCounter = document.getElementById('lost');
+
+let dead = 0;
+let lost = 0;
+
+function getHole(index) {
+  return document.getElementById(`hole${index}`);
+}
+
+function bonk(element) {
+  if (!element.isTrusted) return; 
+
+  if (element.target.matches('.hole_has-mole')) {
+    dead++;
+    deadCounter.textContent = dead;
+
+    if (dead === 10) {
+      alert('Поздравляем! Вы победили!');
+      resetGame();
+    }
+  } else {
+    lost++;
+    lostCounter.textContent = lost;
+
+    if (lost === 5) {
+      alert('К сожалению, вы проиграли!');
+      resetGame();
+    }
+  }
+}
+
+function resetGame() {
+  dead = 0;
+  lost = 0;
+  deadCounter.textContent = dead;
+  lostCounter.textContent = lost;
+}
+
+holes.forEach((hole) => {
+  hole.addEventListener('click', bonk);
+});
