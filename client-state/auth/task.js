31626a1211a71f4910a9b5807905a89e98aed7b3
dmitriydmx <105965325+dmitriydmx@users.noreply.github.com>
2023-10-03 11:48:19 +0900

    second commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,39 +1,53 @@
-function sendForm() {  
+function sendForm(event) {
+  event.preventDefault();
+ 
   let form = document.getElementById('signin__form');
-  let formData = new FormData(form);  
+  let formData = new FormData(form);
+  
   let xhr = new XMLHttpRequest();
   
   xhr.open('POST', form.action, true);
-  xhr.send(formData);  
-  
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.responseType = 'json';
+
   xhr.onload = function() {
-    let response = JSON.parse(xhr.responseText);    
-   
-    if (response.success) {
-      let userId = response.user_id;      
+    if (xhr.status === 200) {
+      let response = xhr.response;
       
-      localStorage.setItem('user_id', userId);      
-      
-      let welcomeBlock = document.getElementById('welcome');
-      welcomeBlock.classList.add('welcome_active');
-      welcomeBlock.querySelector('#user_id').textContent = userId;      
-      
-      form.reset();
+      if (response.success) {
+        let userId = response.user_id;
+        
+        localStorage.setItem('user_id', userId);
+        
+        let welcomeBlock = document.getElementById('welcome');
+        welcomeBlock.classList.add('welcome_active');
+        welcomeBlock.querySelector('#user_id').textContent = userId;
+       
+        form.reset();
+      } else {        
+        alert('Неверный логин/пароль');
+      }
     } else {      
-      alert('Неверный логин/пароль');
+      alert('Ошибка при отправке запроса');
     }
   };
-}
 
+  xhr.onerror = function() {
+    console.error('Ошибка при отправке запроса:', xhr.statusText);
+  };
+
+  xhr.send(new URLSearchParams(formData));
+}
 function signOut() {
   localStorage.removeItem('user_id');
-  
   let welcomeBlock = document.getElementById('welcome');
   welcomeBlock.classList.remove('welcome_active');
 }
 let userId = localStorage.getItem('user_id');
-if (userId) {  
+if (userId) {
   let welcomeBlock = document.getElementById('welcome');
   welcomeBlock.classList.add('welcome_active');
   welcomeBlock.querySelector('#user_id').textContent = userId;
 }
+let form = document.getElementById('signin__form');
+form.addEventListener('submit', sendForm);
